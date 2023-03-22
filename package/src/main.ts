9f87640c07fe0e26652909b724f9eb32d3b48773
giraud florent <florent giraud>
2023-03-22 10:59:01 -0400

    feat: copy MeetingProvider and LoggerProvider from aws react lib

--- package/src/main.ts
+++ package/src/main.ts
@@ -1,9 +1,16 @@
 import { ChimeProvider, useChime } from "./modules/chime/provider"
 import { ChimeMicrophoneProvider, useChimeMicrophone } from "./modules/chime/features/microphone/provider"
+import { MeetingProvider } from "./providers/MeetingProvider"
+import { LoggerProvider } from "./providers/LoggerProvider"
 
 export {
     ChimeProvider,
     useChime,
     ChimeMicrophoneProvider,
     useChimeMicrophone
+}
+
+export {
+    MeetingProvider,
+    LoggerProvider 
 }
---
+++ package/src/providers/LoggerProvider/index.tsx
@@ -0,0 +1,24 @@
+import { ConsoleLogger, Logger, LogLevel } from 'amazon-chime-sdk-js';
+import React, { ReactNode, useContext } from 'react';
+
+const consoleLogger: Logger = new ConsoleLogger(
+  'ChimeSDKReact',
+  LogLevel.INFO
+);
+export const LoggerContext = React.createContext<Logger>(consoleLogger);
+
+interface Props {
+  logger: Logger;
+  children: ReactNode
+}
+
+export const LoggerProvider = ({ logger, children } : Props) => {
+  return (
+    <LoggerContext.Provider value={logger}>{children}</LoggerContext.Provider>
+  );
+};
+
+export const useLogger = (): Logger => {
+  const logger = useContext(LoggerContext);
+  return logger;
+};
